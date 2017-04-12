2d6f9c090b70ad03e11e39e7964f1d1000302c05
Magesh <tkmagesh77@gmail.com>
2017-04-12 12:19:53 +0530

    async assignment

---
+++ 05-async-demo/async-demo.js
@@ -0,0 +1,37 @@
+var app = (function(){
+
+	//Sync
+	function addSync(x,y){
+		console.log('	[Service] processing ', x , ' and ', y);
+		var result = x + y;
+		console.log('	[Service] returning result');
+		return result;
+	}
+
+	function addSyncClient(x,y){
+		console.log('[Client] triggering addSync');
+		var result = addSync(x,y);
+		console.log('[Client] result = ', result);
+	}
+
+	//Async
+	function addAsync(x,y){
+		console.log('	[Service] processing ', x , ' and ', y);
+		setTimeout(function(){
+			var result = x + y;
+			console.log('	[Service] returning result');
+			return result;
+		}, 5000);
+	}
+
+	function addAsyncClient(x,y){
+		console.log('[Client] triggering addAsync');
+		var result = addAsync(x,y);
+		console.log('[Client] result = ', result);
+	}
+
+	return {
+		addSyncClient : addSyncClient,
+		addAsyncClient : addAsyncClient
+	}
+})();
